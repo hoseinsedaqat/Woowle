45ce934a934ebf73ab2f1b7a3fb4b093f9caf3de
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-09-09 11:52:15 +0330

    add/error-page-💀

--- error.vue
+++ error.vue
@@ -1,8 +1,24 @@
 <script setup></script>
 
 <template>
-  <h3>
-    This is Error Page
-    <NuxtLink to="/">Back to Home</NuxtLink>
-  </h3>
+  <section id="error">
+    <main>
+      <div>
+        <NuxtLink to="/">
+          <img src="@/assets/images/WoowleLogo.png" alt="Woowle_Logo" />
+        </NuxtLink>
+      </div>
+      <p>
+        <b>404.</b>
+        <span>That's an error</span>
+      </p>
+      <p>The requested URL was not found on this server.</p>
+      <span>That's all we know</span>
+    </main>
+    <main>
+      <div>
+        <img src="@/assets/images/RobotError.png" alt="Robot_Error" />
+      </div>
+    </main>
+  </section>
 </template>
