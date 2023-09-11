1ac4c4a254447b004029991502dbaa03ef51ad78
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-09-11 11:11:29 +0330

    add/error-componentns refactor/style-scss-files

---
+++ components/ErrorComponent.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <section id="error">
+    <main>
+      <div>
+        <NuxtLink to="/">
+          <img src="@/assets/images/WoowleLogo.png" alt="Woowle_Logo" />
+        </NuxtLink>
+      </div>
+      <p>
+        <b>404.</b>
+        <span>That's an error</span>
+      </p>
+      <p>The requested URL was not found on this server.</p>
+      <span>That's all we know</span>
+    </main>
+    <main>
+      <div>
+        <img src="@/assets/images/RobotError.png" alt="Robot_Error" />
+      </div>
+    </main>
+  </section>
+</template>
--- error.vue
+++ error.vue
@@ -1,24 +1,7 @@
-<script setup></script>
+<script setup>
+import ErrorComponent from "@/components/ErrorComponent.vue";
+</script>
 
 <template>
-  <section id="error">
-    <main>
-      <div>
-        <NuxtLink to="/">
-          <img src="@/assets/images/WoowleLogo.png" alt="Woowle_Logo" />
-        </NuxtLink>
-      </div>
-      <p>
-        <b>404.</b>
-        <span>That's an error</span>
-      </p>
-      <p>The requested URL was not found on this server.</p>
-      <span>That's all we know</span>
-    </main>
-    <main>
-      <div>
-        <img src="@/assets/images/RobotError.png" alt="Robot_Error" />
-      </div>
-    </main>
-  </section>
+  <ErrorComponent></ErrorComponent>
 </template>
